4421d4733455c9b450766d4ce47d6bced0798323
Sebastian Castillo <145416890+SebasRaul2503@users.noreply.github.com>
2025-06-22 01:50:51 -0500

    Primeras impresiones del framework
    Interesante manera de organizar los archivos del proyecto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,29 @@
-import { Controller, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    return this.usersService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.usersService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-    findOne(id:string){
-        return {id: id, name: 'John Doe'}
-    }
+  findOne(id: string) {
+    return { id: id, name: 'John Doe' };
+  }
+
+  create(data: any) {
+    return { message: 'Usuario creado', data };
+  }
+
+  update(id: string, data: any) {
+    return { message: `Usuario ${id} actualizado`, data };
+  }
+
+  remove(id: string) {
+    return { message: `Usuario ${id} eliminado` };
+  }
+
 }
